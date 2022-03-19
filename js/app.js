cf8ce42f8dfd7330debd6fe5ee685f8893e07955
Abduolimbek <Abduolimbek98@gmail.com>
2022-03-20 00:05:21 +0500

    some changes

--- js/app.js
+++ js/app.js
@@ -1,4 +1,24 @@
+const burgerBtn = document.querySelector(".burger__btn");
+const langIcon = document.querySelector(".fa-angle-down");
+const langs = document.querySelectorAll(".media__dropdown .media__lang-text");
 
-document.querySelector('.burger__btn').addEventListener('click', e => {
-  document.querySelector('.navbar').classList.toggle('header__navbar--active')
+burgerBtn.addEventListener("click", e => {
+  document.querySelector(".navbar").classList.toggle("header__navbar--active");
+  burgerBtn.classList.toggle("active");
+  document.body.classList.toggle('hidden');
+});
+
+document.body.addEventListener('click', ({target}) => {
+  if (target.classList.contains('fa-angle-down')) document.querySelector(".media__lang").classList.toggle('active');
+  else {
+    document.querySelector(".media__lang").classList.remove('active');
+  }
 })
+
+langs.forEach(lang => {
+  document.querySelector(".media__lang .media__lang-text").textContent = 'ENG'
+  lang.addEventListener("click", () => {
+    document.querySelector(".media__lang .media__lang-text").textContent = lang.textContent;
+    document.querySelector(".media__lang").classList.remove("active");
+  });
+});
